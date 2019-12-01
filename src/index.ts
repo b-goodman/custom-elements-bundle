178b1b79c2ebb9d78133ad9e56a10a0bea580e5f
Ben Goodman <bgoodman@wolfram.com>
2019-12-01 12:18:42 -0500

    added SplitPane

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import CustomTooltip from "@bgoodman/custom-tooltip";
 import SyntaxHighlight from "@bgoodman/syntax-highlight";
 import TabPane from "@bgoodman/tab-pane";
 import DragPane from "drag-pane";
+import SplitPane from "@bgoodman/split-pane";
 
 
 export {
     CodeEdit,
     CustomTooltip,
     DragPane,
+    SplitPane,
     SyntaxHighlight,
     TabPane,
 }
