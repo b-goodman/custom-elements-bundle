cd8e7c073d86f8e7f0e381cfcd414eb6889fe48b
Ben Goodman <bgoodman@wolfram.com>
2019-12-06 17:00:49 -0500

    added slide-drawer

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ import SyntaxHighlight from "@bgoodman/syntax-highlight";
 import TabPane from "@bgoodman/tab-pane";
 import DragPane from "drag-pane";
 import SplitPane from "@bgoodman/split-pane";
+import SlideDrawer from "@bgoodman/slide-drawer";
 
 
 export {
     CodeEdit,
     CustomTooltip,
     DragPane,
+    SlideDrawer,
     SplitPane,
     SyntaxHighlight,
     TabPane,
